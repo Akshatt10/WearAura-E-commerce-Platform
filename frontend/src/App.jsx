5514aa03a09b1fa14f59c0a13f8b9dced6ebac90
Akshatt10 <akshattyagi1010@gmail.com>
2024-08-24 01:02:34 +0530

    added some changes with tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
